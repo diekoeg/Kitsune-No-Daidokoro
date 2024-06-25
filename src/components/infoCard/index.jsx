386be5abbc340c6bc7098dcac51aa6329116d7b3
Diego Estrada Guadarrama <diego_estrada14@hotmail.com>
2024-06-25 00:11:33 -0600

    Fix: Tamaño de los items del home
    + Se arregla bug que sacaba el texto de los items del home

--- src/components/infoCard/index.jsx
+++ src/components/infoCard/index.jsx
@@ -9,7 +9,7 @@ function InfoCard({img,title,text,color,isRight}) {
                     xl:my-10 ${isRight && 'xl:flex-row-reverse'}`}>
       <Image src={img}  
               className='border-2 xl:absolute rounded-t-3xl xl:rounded-full w-[80%] sm:w-[390px] '/>
-      <div className={`flex border-2 w-[80%] h-auto max-h-[390px] ${color } rounded-b-3xl 
+      <div className={`flex border-2 w-[80%] h-auto  ${color } rounded-b-3xl 
                        xl:h-full xl:rounded-3xl xl:items-center sm:w-full
                        ${isRight ? 'xl:mr-48':'xl:ml-48'}  `}>
         <div className={`py-3 flex flex-col items-center pr-3 pl-3
